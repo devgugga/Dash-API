905b5fb5151b38763cb61c09b6fe5c8e620e5017
devgugga <gustavogomes90104@gmail.com>
2024-12-11 10:44:36 -0300

    🎉 Configura projeto inicial com Spring Boot e Gradle Wrapper
    Adiciona estrutura básica de um projeto Spring Boot com suporte ao Gradle Wrapper. Inclui configurações iniciais, dependências no build.gradle, e arquivos essenciais como .gitignore, application.properties e testes básicos.

---
+++ src/test/java/br/com/gustavgomes/dashapi/DashApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gustavgomes.dashapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DashApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
